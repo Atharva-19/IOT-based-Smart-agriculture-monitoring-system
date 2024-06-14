b0610b16ef6416e027e9411c37e0994c8dffca5d
Atharva-19 <121353179+Atharva-19@users.noreply.github.com>
2024-06-14 16:58:02 +0530

    Update arduino code.cpp

--- arduino code.cpp
+++ arduino code.cpp
@@ -2,26 +2,25 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 
-// Enter Your Bolt Device Credentials
+
 #define API_KEY     "api_key"
 #define DEVICE_ID   "Device_id"
 #define DHTPIN 2   // Defining DHT Senor Pin that is connected with Boltduino
 
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
+
 #define DHTTYPE DHT11   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
 DHT dht = DHT(DHTPIN, DHTTYPE);
 
 void setup() {
-  boltiot.begin(Serial);  //Initialize the Bolt interface over serial UART
-  pinMode(2,INPUT);  //Set pin 2 as the input
+  boltiot.begin(Serial);  
+  pinMode(2,INPUT); 
   dht.begin();
 }
 
 void loop() {
-  // Wait a few seconds between measurements:
+  
   delay(500);
 
   // Read the humidity in %:
@@ -29,6 +28,5 @@ void loop() {
   // Read the temperature as Celsius:
   float temperature = dht.readTemperature();
 
-  // Pushing values on Bolt Cloud
   boltiot.processPushDataCommand(temperature, humidity);
 }
